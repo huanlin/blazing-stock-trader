29d11e329ddbbc0e18a234de9e40e80576882d92
Michael Tsai <huanlin.tsai@gmail.com>
2022-12-24 23:57:45 +0800

    Successfully removed Blazor Server project.

---
+++ src/BlazingStockTrader.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazingStockTrader.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
